d6740752ad29b914e355f981567fcba95b312bee
Nagygyörgy Ádám <nagyradam@gmail.com>
2024-03-05 16:06:34 +0100

    hamburger menu borders

--- chat/src/components/home/home-mobile/mobile-hamburger-menu/mobile-hamburger-menu.component.ts
+++ chat/src/components/home/home-mobile/mobile-hamburger-menu/mobile-hamburger-menu.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { PageState } from '../../../../models/ui.state'
+import { CommonModule } from '@angular/common'
 
 @Component({
   standalone: true,
   selector: 'app-mobile-hamburger-menu',
   templateUrl: './mobile-hamburger-menu.component.html',
-  styleUrls: ['./mobile-hamburger-menu.component.css']
+  styleUrls: ['./mobile-hamburger-menu.component.css'],
+  imports: [CommonModule]
 })
 export class MobileHamburgerMenuComponent {
-  @Output() changePageEvent = new EventEmitter<PageState>()
   PageState = PageState
+
+  @Input() selectedPage?: PageState
+  @Output() changePageEvent = new EventEmitter<PageState>()
 
   changePage (page: PageState): void {
     this.changePageEvent.emit(page)
