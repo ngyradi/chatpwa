f02015aa578494cc73420738bbe816ebe9151fb5
Nagygyörgy Ádám <nagyradam@gmail.com>
2024-03-06 10:01:53 +0100

    removed old user login form

--- chat/src/components/user/user-login/user-login.component.ts
+++ chat/src/components/user/user-login/user-login.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, type OnInit } from '@angular/core'
 import { PageContainerComponent } from '../../page-container/page-container.component'
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component'
 import { RouterLink, RouterModule } from '@angular/router'
 import { UserService } from '../../../services/user.service'
 import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login'
@@ -10,7 +9,7 @@ import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login'
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css'],
-  imports: [PageContainerComponent, UserLoginFormComponent, RouterModule, RouterLink, GoogleSigninButtonModule]
+  imports: [PageContainerComponent, RouterModule, RouterLink, GoogleSigninButtonModule]
 })
 export class UserLoginComponent implements OnInit {
   constructor (@Inject(UserService) private readonly userService: UserService) { }
