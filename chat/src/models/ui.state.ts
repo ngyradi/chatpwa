0feae869ac9f8c13e784942377c7f7c6ccc41776
Nagygyörgy Ádám <nagyradam@gmail.com>
2024-03-06 09:57:33 +0100

    state comments

--- chat/src/models/ui.state.ts
+++ chat/src/models/ui.state.ts
@@ -1,9 +1,11 @@
+// Used in the mobile home screen for switching pages
 export enum PageState {
   ROOMS,
   CHAT,
   USERS,
 }
 
+// Used to store if the user is talking in a room or sending a private message
 export enum ChatState {
   ROOM,
   PRIVATE,
